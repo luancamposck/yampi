fee0195c2458461b5f7cf687f236b02b64947e6e
LuanCKRG <luancamposck@gmail.com>
2025-10-01 00:09:12 -0300

    Initial commit

---
+++ components/generic/DiscountTag.vue
@@ -0,0 +1,42 @@
+<template>
+    <div
+        v-if="value > 0"
+        class="discount-tag"
+    >
+        <span>{{ formattedValue }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DiscountTag',
+
+    props: {
+        value: {
+            type: Number,
+            default: 0,
+        },
+    },
+
+    computed: {
+        formattedValue() {
+            return Math.round(this.value);
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    @media only screen and (max-width: 700px) {
+        .discount-tag {
+            position: absolute;
+            top: 0;
+            right: 0;
+            z-index: 1;
+
+            span {
+                font-size: 11px;
+            }
+        }
+    }
+</style>
